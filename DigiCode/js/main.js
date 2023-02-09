d193fd51abcd42acc6ea6a010b3b6261e2e0d595
Yoav Cartozo <cartozoy1@gmail.com>
2023-02-09 15:51:13 +0100

    HW-digiCode-clear

--- DigiCode/js/main.js
+++ DigiCode/js/main.js
@@ -1,28 +1,57 @@
-var btn1 = document.createElement("button");
-btn1.innerText = "1\n\n .";
-var btn2 = document.createElement("button");
-btn2.innerText = "2\n\n A, B, C";
-var btn3 = document.createElement("button");
-btn3.innerText = "3\n\n D, E, F";
-var btn4 = document.createElement("button");
-btn4.innerText = "4\n\n G, H, I";
-var btn5 = document.createElement("button");
-btn5.innerText = "5\n\n J, K, L";
-var btn6 = document.createElement("button");
-btn6.innerText = "6\n\n M, N, O";
-var btn7 = document.createElement("button");
-btn7.innerText = "7\n\n P, Q, R, S";
-var btn8 = document.createElement("button");
-btn8.innerText = "8" + "\n\nT, U, V";
-var btn9 = document.createElement("button");
-btn9.innerText = "9\n\n W, X, Y, Z";
-var btn0 = document.createElement("button");
-btn0.innerText = "0\n\n .";
+const buttons = [
+  { number: "1", letters: "." },
+  { number: "2", letters: "A, B, C" },
+  { number: "3", letters: "D, E, F" },
+  { number: "4", letters: "G, H, I" },
+  { number: "5", letters: "J, K, L" },
+  { number: "6", letters: "M, N, O" },
+  { number: "7", letters: "P, Q, R, S" },
+  { number: "8", letters: "T, U, V" },
+  { number: "9", letters: "W, X, Y, Z" },
+  { number: "0", letters: "." },
+];
 
-document
-  .getElementById("table")
-  .append(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0);
+const code = "1706";
+var typedCode = "";
+var tries = 3;
+var timeout;
 
-document.getElementById("main").append(document.getElementById("table"))
+const table = document.getElementById("table");
 
-// btn1.addEventListener("click", function ()
+buttons.forEach(function ({ number, letters }) {
+  const btn = document.createElement("button");
+  btn.innerText = number + "\n\n" + letters;
+  btn.addEventListener("click", function () {
+    clearTimeout(timeout);
+    btn.style.backgroundColor = "aqua";
+    typedCode += number;
+    timeout = setTimeout(reset, 3000);
+    if (typedCode.length === 4) {
+      verifyCode();
+    }
+  });
+  table.append(btn);
+});
+
+document.getElementById("main").append(table);
+
+function verifyCode() {
+  if (typedCode === code) {
+    alert("OPEN !!!");
+  } else {
+    tries--;
+    if (tries === 0) {
+      alert("You locked your phone");
+    } else {
+      alert(tries + " tries left");
+    }
+  }
+  reset();
+}
+
+function reset() {
+  typedCode = "";
+  document.querySelectorAll("button").forEach(function (btn) {
+    btn.style.backgroundColor = "";
+  });
+}
